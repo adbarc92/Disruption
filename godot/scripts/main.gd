7973e9d2e84f4e173c6598f2093411c78c79560d
adbarc92 <adbarclay92@gmail.com>
2026-03-23 13:55:36 -0600

    Remove Start Game button, add dialogue system design spec
    Remove the placeholder Start Game button from the main menu since it
    duplicated Test Exploration. Add the design spec for the prototype
    dialogue system covering data format, logic layer, and presentation.

--- godot/scripts/main.gd
+++ godot/scripts/main.gd
@@ -6,15 +6,8 @@ func _ready() -> void:
 	GameManager.change_state(GameManager.GameState.MAIN_MENU)
 
 	# Connect button signals
-	$CanvasLayer/StartButton.pressed.connect(_on_start_pressed)
 	$CanvasLayer/TestCombatButton.pressed.connect(_on_test_combat_pressed)
 	$CanvasLayer/TestExplorationButton.pressed.connect(_on_test_exploration_pressed)
-
-
-func _on_start_pressed() -> void:
-	print("Starting game - Opening sequence")
-	# TODO: Start with cutscene/opening sequence
-	GameManager.transition_to_scene("res://scenes/exploration/test_exploration.tscn")
 
 
 func _on_test_combat_pressed() -> void:
